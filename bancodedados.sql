172d5ee6adda2569068e23aa5209e6772bc4d94f
Paulo <paulodemetrio1982@gmail.com>
2023-11-21 14:51:49 -0300

    adcionando ponto no final das frases

--- bancodedados.sql
+++ bancodedados.sql
@@ -7,13 +7,13 @@ default collate utf8mb4_general_ci;
 -- Criar uma tabela desse Banco de Dados
 
 create table pessoas (
-	id int not null auto_increment, -- chave primária, não pode ser nulo e incrementar um numero caso nada for digitado
-	nome varchar(30) not null, -- nenhum nome pode ficar vazio
-    nascimento date, -- cadastrar data de nascimento
-    sexo enum('M','F'), -- enum especifica masculino ou feminino
-    peso decimal(5,2), -- aqui o 5,2 siginifica 5 numeros e 2 casas decimais
-    altura decimal(3,2), -- 3 numeros com duas casas decimais
-    nacionalidade varchar(20) default 'Brasil', -- especifiquei o Brasil como default
+	id int not null auto_increment, -- chave primária, não pode ser nulo e incrementar um numero caso nada for digitado.
+	nome varchar(30) not null, -- nenhum nome pode ficar vazio.
+    nascimento date, -- cadastrar data de nascimento.
+    sexo enum('M','F'), -- enum especifica masculino ou feminino.
+    peso decimal(5,2), -- aqui o 5,2 siginifica 5 numeros e 2 casas decimais.
+    altura decimal(3,2), -- 3 numeros com duas casas decimais.
+    nacionalidade varchar(20) default 'Brasil', -- especifiquei o Brasil como default.
     primary key (id)
     ) default charset = utf8mb4;
     
